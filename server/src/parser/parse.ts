2ba0f15fb1d24697c0405c9fae3efc2765ee37d5
Viktor Rognås <viktor.rognas@gmail.com>
2023-10-13 08:23:30 +0200

    Rename `nmtran.ne` to `nmtran-grammar.ne`

--- server/src/parser/parse.ts
+++ server/src/parser/parse.ts
@@ -1,5 +1,5 @@
 const nearley = require("nearley");
-const grammar = require("./nmtran.js");
+const grammar = require("./nmtran-grammar.js");
 
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
 
