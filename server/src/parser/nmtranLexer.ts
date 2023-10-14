0a72919e03aea006da7c300c73cce22933d5d9e8
Viktor Rognås <viktor.rognas@gmail.com>
2023-10-14 13:40:51 +0200

    Create nmtranLexer.ts

---
+++ server/src/parser/nmtranLexer.ts
@@ -0,0 +1,218 @@
+import * as moo from "moo";
+import * as fs from "fs/promises";
+import { validControlRecords } from "../constants";
+
+const mainRules: moo.Rules = {
+  WS: /[ \t]+/,
+  comment: /;.*?$/,
+  math_op: ['*', '/', '+', '-', '**'],
+  logical_op: ['.NOT.', '.AND.', '.OR.', '.EQ.', '.NE.', '.EQN.', '.NEN.', '.LT.', '.LE.', '.GT.', '.GE.', '==', '/=', '<', '<=', '>', '>='],
+  assign: '=',
+  digit:  { match: /[0-9]+/, value: str => Number(str) as any },
+  identifier: {
+    match: /[a-zA-Z][a-zA-Z0-9_]*/,
+    type: moo.keywords({
+      'keyword-if': ['IF', 'THEN', 'ELSE', 'ELSEIF', 'ENDIF'],
+      'keyword-while': ['DO WHILE', 'ENDDO'],
+      'keyword-other': ['CALL', 'WRITE', 'PRINT', 'RETURN', 'OPEN', 'CLOSE', 'REWIND', 'EXIT'],
+      'fortran_builtin': ['LOG10', 'LOG', 'EXP', 'SQRT', 'SIN', 'COS', 'TAN', 'ASIN', 'ACOS', 'ATAN', 'ABS', 'INT', 'MIN', 'MAX', 'MOD'],
+      'nonmem_protected': ['PLOG', 'PEXP'],
+      'nonmem_builtin': ['PHI', 'GAMLN'],
+    })
+  },
+  lparen: '(',
+  rparen: ')',
+  NL: { match: /\n/, lineBreaks: true },
+  myError: moo.error,
+};
+
+// Dynamically generate control records and switch lexical states
+const controlRecords: moo.Rules = Object.fromEntries(
+  validControlRecords.map(record => [
+    record.slice(1), // Remove the '$' for the key
+    { match: new RegExp(`\\${record}`), next: record.slice(1) } // Escape the '$' for the regex
+  ])
+);
+
+const customRules: { [key: string]: moo.Rules } = {
+  ABBREVIATED: {
+  },
+  AES: {
+  },
+  AESINITIAL: {
+  },
+  ANNEAL: {
+  },
+  BIND: {
+  },
+  CHAIN: {
+  },
+  CONTR: {
+  },
+  COVARIANCE: {
+  },
+  COVR: {
+  },
+  DATA: {
+  },
+  DEFAULT: {
+  },
+  DES: {
+  },
+  DESIGN: {
+  },
+  ERROR: {
+  },
+  ESTIMATION: {
+  },
+  ESTIMATE: {
+  },
+  ESTM: {
+  },
+  ETAS: {
+  },
+  PHIS: {
+  },
+  FORMAT: {
+  },
+  INDEX: {
+  },
+  INDXS: {
+  },
+  INFN: {
+  },
+  INPUT: {
+  },
+  LEVEL: {
+  },
+  MIX: {
+  },
+  MODEL: {
+  },
+  MSFI: {
+  },
+  NONPARAMETRIC: {
+  },
+  OLKJDF: {
+  },
+  OMEGA: {
+  },
+  OMEGAP: {
+  },
+  OMEGAPD: {
+  },
+  OMIT: {
+  },
+  OVARF: {
+  },
+  PK: {
+  },
+  PRED: {
+  },
+  PRIOR: {
+  },
+  PROBLEM: {
+    problem_text: {
+      match: /[^\n]{1,72}/,  // Captures up to 72 characters that are not a newline
+      lineBreaks: false,     // We don't want to capture line breaks
+    }
+  },
+  RCOV: {
+  },
+  RCOVI: {
+  },
+  SCATTERPLOT: {
+  },
+  SIGMA: {
+  },
+  SIGMAP: {
+  },
+  SIGMAPD: {
+  },
+  SIMULATION: {
+  },
+  SIMULATE: {
+  },
+  SIML: {
+  },
+  SIZES: {
+  },
+  SLKJDF: {
+  },
+  SUBROUTINES: {
+  },
+  SUPER: {
+  },
+  SVARF: {
+  },
+  TABLE: {
+  },
+  THETA: {
+  },
+  THI: {
+  },
+  THETAI: {
+  },
+  THETAP: {
+  },
+  THETAPV: {
+  },
+  THETAR: {
+  },
+  THR: {
+  },
+  TOL: {
+  },
+  TTDF: {
+  },
+  WARNINGS: {
+  },
+};
+
+// Initialize the lexer
+const nmtranLexer = moo.states({
+  main: {
+    ...mainRules,
+    ...controlRecords
+  },
+  // Dynamically generate states for each control record
+  ...Object.fromEntries(
+    validControlRecords.map(record => [
+      record.slice(1), // State name
+      {
+        ...customRules[record.slice(1)],
+        ...mainRules,
+        ...controlRecords
+      }
+    ])
+  )
+})
+
+// const nmtranLexer: moo.Lexer = moo.compile(lexerRules);
+
+async function main(inputFile: string, outputFile: string) {
+  let logText = '';
+  try {
+    const controlStream = (await fs.readFile(inputFile)).toString();
+    nmtranLexer.reset(controlStream);
+    while (true) {
+      const token = nmtranLexer.next();
+      if (!token) {
+        break;
+      }
+      logText += JSON.stringify(token) + '\n';
+    }
+    await fs.writeFile(outputFile, logText);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err.stack);
+    } else {
+      console.log('An unknown error occurred:', err);
+    }
+  }  
+}
+
+main(
+  './test/minimal.mod',
+  './test/lexer-log.json'
+);
