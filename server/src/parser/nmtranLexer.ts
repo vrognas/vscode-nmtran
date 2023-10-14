f0cc11c3d9c705d00c03a8c47ebaa7212b0d9967
Viktor Rognås <viktor.rognas@gmail.com>
2023-10-14 16:41:12 +0200

    Add lexical rules for `$DATA`

--- server/src/parser/nmtranLexer.ts
+++ server/src/parser/nmtranLexer.ts
@@ -2,7 +2,8 @@ import * as moo from "moo";
 import * as fs from "fs/promises";
 import {
   validControlRecords,
-  validSizesOptions
+  validSizesOptions,
+  reservedDataItemLabels
 } from "../constants";
 
 const mainRules: moo.Rules = {
@@ -60,6 +61,54 @@ const customRules: { [key: string]: moo.Rules } = {
   COVR: {
   },
   DATA: {
+    // For single character IGNORE or IGN like IGNORE=c, IGN=c, IGNORE='c' or IGNORE="c"
+    data_ignore_char: {
+      match: /IGN(?:ORE)?\s*=\s*(?:'[^'\n]'|"[^"\n]"|[a-zA-Z0-9@#])/,
+      value: (str: any) => str.split('=')[1].trim().replace(/['"]/g, '')
+    },
+    // For alphabetic character IGNORE or IGN like IGNORE=@ or IGN=@
+    data_ignore_alpha: {
+      match: /IGN(?:ORE)?\s*=\s*@/,
+      value: (str: any) => str.split('=')[1].trim()
+    },
+    // For IGNORE=(list) or ACCEPT=(list)
+    data_ignore_accept_list: {
+      match: /(?:IGN(?:ORE)?|ACC(?:EPT)?)\s*=?\s*\((?:[a-zA-Z0-9_'".NEQGLT\/=\-\s,<>=]+)\)/,
+      value: (str: any) => {
+        const type = str.startsWith('IGN') ? 'IGNORE' : 'ACCEPT';
+        const listStr = str.match(/\((.*)\)/)![1];
+        const list = listStr.split(',').map((s: string) => s.trim());
+        return { type, list };
+      }
+    } as any,
+    data_null: {
+      match: /NULL\s*=\s*(?:[a-zA-Z0-9_]+)/, // NULL= followed by its value
+      value: (str: any) => str.split('=')[1].trim() // Extract the value after the equal sign
+    },
+    data_pred_ignore: /PRED_IGNORE_DATA/, // Exact match for PRED_IGNORE_DATA
+    data_nowide: /NOWIDE/, // Exact match for NOWIDE
+    data_wide: /WIDE/, // Exact match for WIDE
+    data_checkout: /CHECKOUT/, // Exact match for CHECKOUT
+    // Match filenames that are surrounded by quotes or not, up to 80 characters.
+    // Special characters are allowed if the filename is quoted.
+    data_format: {
+      match: /\((?:[^)]+)\)/, // Anything inside parentheses
+      value: (str: any) => str.slice(1, -1).trim() // Remove parentheses and trim
+    },
+    data_filename: {
+      match: /'[^'\n]{1,80}'|"[^"\n]{1,80}"|[^ ,;()='"\n]{1,80}/,
+      value: (str: any) => {
+        // Remove quotes if present
+        if (str.startsWith("'") || str.startsWith('"')) {
+          return str.slice(1, -1);
+        }
+        return str;
+      }
+    },
+    unknown_option: {
+      match: /[a-zA-Z0-9_\-\/=]+/, // Alphabets, numbers, underscores, hyphens, slashes, and equal signs
+      value: (str: any) => str.trim() // Trim any extra spaces
+    },
   },
   DEFAULT: {
   },
@@ -88,6 +137,21 @@ const customRules: { [key: string]: moo.Rules } = {
   INFN: {
   },
   INPUT: {
+    inputDataItemLabelDropped: {
+      match: /[a-zA-Z_][a-zA-Z0-9_]{0,19}(?=\s*=\s*(?:DROP|SKIP))/,
+      type: "inputDataItemLabelDropped"
+    } as unknown as moo.Rule, // Hack to avoid TypeScript error
+    inputDataItemLabel: {
+      match: /[a-zA-Z_][a-zA-Z0-9_]{0,19}/, // Up to 20 characters, starts with a letter
+      type: moo.keywords({
+        'reservedDataItemLabel': reservedDataItemLabels,
+      })
+    },
+    input_item_form: {
+      // This regex tries to capture both "A=B" and "B" forms
+      // It assumes that the "=" sign and label names have no spaces around them
+      match: /(?:[a-zA-Z_][a-zA-Z0-9_]{0,19})=(?:[a-zA-Z_][a-zA-Z0-9_]{0,19})|(?:[a-zA-Z_][a-zA-Z0-9_]{0,19})/,
+    }
   },
   LEVEL: {
   },
@@ -148,11 +212,10 @@ const customRules: { [key: string]: moo.Rules } = {
         'sizes_constant': validSizesOptions,
       })
     },
-
     sizes_value: {
       match: /-?\d+/, // Values (positive, zero, or negative)
       value: (str: any) => Number(str),
-    } as any,
+    } as unknown as moo.Rule, // Hack to avoid TypeScript error
   },
   SLKJDF: {
   },
